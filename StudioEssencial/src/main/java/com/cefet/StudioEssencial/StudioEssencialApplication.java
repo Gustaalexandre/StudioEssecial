df2c19197315d1ce032a4ec30650c90eb5429ab7
Gustaalexandre <153231777+Gustaalexandre@users.noreply.github.com>
2025-06-11 07:42:27 -0300

    Add files via upload

---
+++ StudioEssencial/src/main/java/com/cefet/StudioEssencial/StudioEssencialApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.StudioEssencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioEssencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioEssencialApplication.class, args);
+	}
+
+}
---
+++ StudioEssencial/src/test/java/com/cefet/StudioEssencial/StudioEssencialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.StudioEssencial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioEssencialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
