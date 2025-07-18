89ac11bfaa9264ea46d12138ac7e2b46fc8b9e09
maneloliver33 <carlosmanuoliveiradias@gmail.com>
2025-07-18 07:29:19 -0300

    criptografia de senha

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,4 +14,4 @@ INSERT INTO tb_agendamento(pessoa_id, procedimento_id, nm_produto, quantidade, d
 INSERT INTO tb_pagamento(data_pagamento, data_vencimento, valor, forma_pagamento, numero_parcela, agendamento_id) VALUES ('2025-07-04', '2025-07-04', 40.00, 'DINHEIRO', 1, 1), ('2025-07-04', '2025-07-04', 35.00, 'CARTAO', 1, 2), ('2025-07-05', '2025-07-05', 25.00, 'PIX', 1, 3);
 
 -- Usuários (OK)
-INSERT INTO tb_usuario(login, senha, nivel_acesso, pessoa_id) VALUES ('AAA', '$2a$10$ZQDZDXqV/LKQTq13NKJ2aeUmrxV787/U1BSEfXS.pGPu0Sd9CgYX6', 'FUNCIONARIO', 1), ('BBB', '222', 'CLIENTE', 2), ('CCC', '333', 'FUNCIONARIO', 3);
+INSERT INTO tb_usuario(login, senha, nivel_acesso, pessoa_id) VALUES ('AAA', '$2a$10$ZQDZDXqV/LKQTq13NKJ2aeUmrxV787/U1BSEfXS.pGPu0Sd9CgYX6', 'FUNCIONARIO', 1), ('BBB', '$2a$10$ZQDZDXqV/LKQTq13NKJ2aeUmrxV787/U1BSEfXS.pGPu0Sd9CgYX6', 'CLIENTE', 2), ('CCC', '$2a$10$ZQDZDXqV/LKQTq13NKJ2aeUmrxV787/U1BSEfXS.pGPu0Sd9CgYX6', 'FUNCIONARIO', 3);
