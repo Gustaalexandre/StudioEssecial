42b8753592b9f23111ec2d15ed61f960d70d214e
Gustaalexandre <galexandre389@gmail.com>
2025-07-25 07:37:49 -0300

    cors atualzação

--- src/main/java/com/cefet/StudioEssencial/config/SecurityConfig.java
+++ src/main/java/com/cefet/StudioEssencial/config/SecurityConfig.java
@@ -117,7 +117,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://studioessencialpp.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
